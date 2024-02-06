c799ae0c820cbf8b686da3bf8f5268377418e5f3
julianstore <today.julian.lim@gmail.com>
2024-02-06 08:46:35 -0800

    add register route

--- Backend/controllers/Auth.js
+++ Backend/controllers/Auth.js
@@ -93,3 +93,71 @@ export const changePassword = async (req, res) => {
     res.status(400).json({ msg: error.message });
   }
 };
+
+// Register function
+export const Register = async (req, res) => {
+  const {
+    nik,
+    nama_pegawai,
+    username,
+    password,
+    confPassword,
+    jenis_kelamin,
+    jabatan,
+    tanggal_masuk,
+    status,
+    hak_akses,
+  } = req.body;
+
+  if (password !== confPassword) {
+    return res.status(400).json({ msg: 'Password dan Konfirmasi Password Tidak Cocok' });
+  }
+
+  if (!req.files || !req.files.photo) {
+    return res.status(400).json({ msg: 'Upload Foto Gagal Silahkan Upload Foto Ulang' });
+  }
+
+  const file = req.files.photo;
+  const fileSize = file.data.length;
+  const ext = path.extname(file.name);
+  const fileName = file.md5 + ext;
+  const url = `${req.protocol}://${req.get('host')}/images/${fileName}`;
+  const allowedTypes = ['.png', '.jpg', '.jpeg'];
+
+  if (!allowedTypes.includes(ext.toLowerCase())) {
+    return res.status(422).json({ msg: 'File Foto Tidak Sesuai Dengan Format' });
+  }
+
+  if (fileSize > 2000000) {
+    return res.status(422).json({ msg: 'Ukuran Gambar Harus Kurang Dari 2 MB' });
+  }
+
+  file.mv(`./public/images/${fileName}`, async (err) => {
+    if (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+
+    const hashPassword = await argon2.hash(password);
+
+    try {
+      await DataPegawai.create({
+        nik: nik,
+        nama_pegawai: nama_pegawai,
+        username: username,
+        password: hashPassword,
+        jenis_kelamin: jenis_kelamin,
+        jabatan: jabatan,
+        tanggal_masuk: tanggal_masuk,
+        status: status,
+        photo: fileName,
+        url: url,
+        hak_akses: hak_akses,
+      });
+
+      res.status(201).json({ success: true, message: 'Registrasi Berhasil' });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ success: false, message: error.message });
+    }
+  });
+};
